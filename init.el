1c79cb69dd8ab2d4a0ab441362d15eadca222858
wacken <sebastianwalchi@gmail.com>
2021-02-13 21:59:15 +0100

    with japanese

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
-       ;;japanese
+       japanese
 
        :completion
        (company +tng)                   ; the ultimate code completion backend +childframe
