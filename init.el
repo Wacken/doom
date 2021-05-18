27ad1ed74be1f623d9dd53c550a5c2beaf25fbf8
wacken <sebastianwalchi@gmail.com>
2021-05-18 12:25:47 +0200

    remove and comment unneeded modules

--- init.el
+++ init.el
@@ -106,7 +106,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;cc                ; c/c++/obj-c madness
-       (clojure)           ; java with a lisp
+       ;; (clojure)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
